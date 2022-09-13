2b7135e2cafd28b3819c40be6efae61569e38699
Piotr Markiewicz <piotr.markiewicz@ttms.pl>
2022-09-13 20:21:34 +0200

    pokemon api used

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import TheView from "./components/TheView.vue";
+import MyPokemon from "./components/MyPokemon.vue";
+export default {
+  components: {
+    TheView,
+    MyPokemon,
+  },
+};
+</script>
+
+<template>
+  <h1>Ejoppa!</h1>
+  <TheView element="div">Hello from Vue!</TheView>
+  <MyPokemon />
+</template>
---
+++ src/components/MyPokemon.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      pokemons: [],
+    };
+  },
+  methods: {
+    async getPokemon() {
+      const data = await fetch("https://pokeapi.co/api/v2/pokemon");
+      this.pokemons = (await data.json()).results;
+    },
+  },
+};
+</script>
+
+<template>
+  <button @click="getPokemon">Get Pokemon</button>
+  <div v-for="(pokemon, idx) in pokemons" :key="idx">
+    <h4>{{ pokemon.name }}</h4>
+  </div>
+</template>
